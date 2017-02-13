075e2b82612a04ce7962eeea6983dba678636d73
superkevingit <superkevingit@gmail.com>
2017-02-13 22:59:37 +0800

    change something not important

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
     <footer class="footer wrapper center">
-        &copy; 2015 <b><?php $this->options->title() ?></b>. All rights reserved. Proudly powered by <a href="http://typecho.org" target="_blank">Typecho</a>.
+        &copy; 2017 <b><?php $this->options->title() ?></b> <a href="http://typecho.org" target="_blank">By Typecho && Vultr</a>.
     </footer>
     <div class="btm-search">
         <form class="search" method="post" action="/" role="search">
